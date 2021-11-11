e6b35b6d701dfdd9a20529c47c7e075ff9f8121c
fjbernalc <fjbernalc@users.noreply.github.com>
2021-11-11 02:05:27 +0000

    deploy: 49ce1cbc9c400cd4296a728f0678947d25ca4f5c

--- sketches/zollner.js
+++ sketches/zollner.js
@@ -1,10 +1,7 @@
-var img;   // Definimos una variable para almacenar la imagen
-function preload() {   // Añadimos una nueva función que nos 
-                       // carga la imagen
-  img = loadImage("/vc/sketches/Zollner.jpg");
-}
+let img;
 function setup() {
-  createCanvas(480, 120);
+  createCanvas(400, 400);
+  img = loadImage("/Visual-Computing-2021-2S/sketches/Zollner.jpg");
 }
 function draw() {
   image(img, 0, 0); // Dibuja la imagen
