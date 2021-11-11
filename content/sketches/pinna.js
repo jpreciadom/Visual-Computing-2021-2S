86781e50fcacfc39aa8795fdcb90c24f9a3d9b0f
Gustavo Galvez <ggalvezb@unal.edu.co>
2021-11-10 23:41:14 -0500

    Nuevo ajuste

--- content/sketches/pinna.js
+++ content/sketches/pinna.js
@@ -1,10 +1,7 @@
-var img;   // Definimos una variable para almacenar la imagen
-function preload() {   // Añadimos una nueva función que nos 
-                       // carga la imagen
-  img = loadImage("/Visual-Computing-2021-2S/sketches/pinna.jpg");
-}
+let img;
 function setup() {
   createCanvas(560, 560);
+  img = loadImage("/Visual-Computing-2021-2S/sketches/pinna.jpg");
 }
 function draw() {
   image(img, 0, 0); // Dibuja la imagen
