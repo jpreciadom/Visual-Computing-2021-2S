60482e88ee731205dfaf6f7401af51db1fdab1b2
Francisco Bernal <fjbernalc@unal.edu.co>
2021-11-10 20:10:56 -0500

    changes

--- content/docs/shortcodes/Team/ilusiones.md
+++ content/docs/shortcodes/Team/ilusiones.md
@@ -1,4 +1,4 @@
 ## Ilusión de Zollner
 Bastan unas pocas líneas para confundirnos. Como en esta ilusión descubierta por el astrofísico alemán Johann Karl Friedrich Zöllner en 1860. A pesar de que estas rectas son paralelas, parecen inclinarse por influencia de las líneas diagonales.
 
-{{< p5-iframe sketch="vc\content\sketches\zollner.js" lib1="https://cdn.jsdelivr.net/gh/freshfork/p5.EasyCam@1.2.1/p5.easycam.js" width="725" height="625" >}}
+{{< p5-iframe sketch="vc\content\sketches\zollner.js" >}}
--- content/sketches/zollner.js
+++ content/sketches/zollner.js
@@ -1,11 +1,8 @@
-var img;   // Definimos una variable para almacenar la imagen
-function preload() {   // Añadimos una nueva función que nos 
-                       // carga la imagen
-  img = loadImage("/vc/sketches/Zollner.jpg");
-}
+let img;
 function setup() {
-  createCanvas(480, 120);
+  createCanvas(720, 400);
+  img = loadImage("/Visual-Computing-2021-2S/sketches/Zollner.jpg");
 }
 function draw() {
-  image(img, 0, 0); // Dibuja la imagen
+  image(img, img.width, img.height); // Dibuja la imagen
 }
