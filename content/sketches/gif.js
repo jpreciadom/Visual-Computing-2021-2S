e693099d1c3858be5c1727f45fc8da4a682b88d5
Sarah Lancheros Soler <32717957+slancheross@users.noreply.github.com>
2021-12-13 21:52:21 -0500

    Complemento punto 2

---
+++ content/sketches/gif.js
@@ -0,0 +1,20 @@
+var gif_loadImg, gif_createImg;
+
+function preload() {
+  gif_createImg = createImg("circle.gif");
+}
+
+function setup() {
+  createCanvas(500, 700);
+  background(0);
+}
+
+function draw() {
+  // loads only first frame
+  image(gif_loadImg, 50, 50);
+  
+  // updates animation frames by using an html
+  // img element, positioning it over top of
+  // the canvas.
+  gif_createImg.position(50, 350);
+}
