32c429f1524443acbbc08c3b50256c0ccf96239e
slancheross <slancheross@users.noreply.github.com>
2021-12-14 03:06:21 +0000

    deploy: a4fe469df63ab8e2907ff68ecffd9c65aafba2ca

--- sketches/gif.js
+++ sketches/gif.js
@@ -1,10 +1,14 @@
 var gif_loadImg, gif_createImg;
 
 function preload() {
-  gif_loadImg = loadImage("circle.gif");
-  gif_createImg = createImg("circle.gif");
+  gif_loadImg = loadImage("/Visual-Computing-2021-2S/sketches/circle.gif");
+  gif_createImg = createImg("/Visual-Computing-2021-2S/sketches/circle.gif");
 }
 
 function setup() {
   createCanvas(589, 589);
 }
+
+function draw() {
+    image(gif_loadImg, 0, 0); // Dibuja la imagen
+  }
